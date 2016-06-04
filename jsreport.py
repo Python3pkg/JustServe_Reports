2a3a264e3ae9307ac9881bce4a251cde984259c4
Andrew Badstubner <andrew@amplifytechgroup.com>
2016-06-03 22:43:02 -0400

    Non-working version with splinter. Had a hard time finding several page elements.

--- jsreport.py
+++ jsreport.py
@@ -2,13 +2,39 @@
 """
 Helper script to pull data from https://www.justserve.org.
 """
+from splinter import Browser
+
 
 def get_project_count(zipcode, radius="5", driver="firefox"):
     """
     Returns the number of projects on JustServe.org at the given zipcode,
     within the given radius.
     """
-    pass
+    num_projects = "Not found"
+    with Browser(driver) as browser:
+        browser.visit('https://www.justserve.org/')
+        browser.screenshot("1.png")
+        search_box = browser.find_by_css("input.form__search__input").first
+        search_box.fill(zipcode)
+        #browser.find_by_css("a.js-toggle-form-filters").first.click()
+        
+        #radius_select = browser.find_by_css("select.js-miles").first
+        #radius_select.click()
+        #radius_select.select(radius)
+        #browser.select("js-miles", radius)
+        #browser.find_option_by_text("%s miles" % radius).first.click()
+        #for option in radius_select.find_by_tag("option"):
+        #    if option.value == radius:
+        #        option.click()
+        #        break
+
+        #browser.find_by_css("input.js-loadable").first.click()
+        browser.screenshot("2.png")
+        #btn = browser.find_by_css("input.js-loadable").first
+        #browser.execute_script("arguments[0].click();", btn)
+        
+        #num_projects = browser.find_by_css("project-count__num").first.value
+    return num_projects
 
 
 
@@ -18,7 +44,7 @@ if __name__=='__main__':
     import argparse
     parser = argparse.ArgumentParser(
             description="Retrieve the number of JustServe projects at a zipcode.")
-    parser.add_argument("-z", "--zipcode", required=True, 
+    parser.add_argument("zipcode", 
             help="The five digit zipcode at the center of the search radius.")
     parser.add_argument("-r", "--radius", default="5",
             choices=["5", "10", "15", "25", "50", "75"],
@@ -28,4 +54,6 @@ if __name__=='__main__':
                 "zope.testbrowser", "django"],
             help="The WebDriver to use. Defaults to firefox.")
     args = parser.parse_args()
+    project_count = get_project_count(args.zipcode, args.radius, args.driver)
+    print "Projects:", project_count
 
