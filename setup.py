e367191d3275377a91cbaa9370fd01d26e2284f4
Andrew Badstubner <andrew@amplifytechgroup.com>
2016-06-10 09:39:48 -0400

    Added a manifest and some classifiers to the setup script. Incremented the version, too.

--- setup.py
+++ setup.py
@@ -1,8 +1,25 @@
 from setuptools import setup
 
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
 setup(name='jsreport',
-      version='0.2.1',
+      version='0.2.2',
       description='Simple tool to pull basic reporting data out of JustServe (https://www.justserve.org/).',
+      long_description=readme(),
+      classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: Religion',
+          'Intended Audience :: Other Audience',
+          'License :: OSI Approved :: MIT License',
+          'Natural Language :: English',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python :: 2.7',
+          'Topic :: Other/Nonlisted Topic',
+      ]
       url='https://github.com/abadstubner/JustServe_Reports',
       author='abadstubner',
       author_email='andrew+pypi@badstubner.com',
